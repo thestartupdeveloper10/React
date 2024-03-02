50c990c1389790beb0ceb1b0c3d55745278b703b
ibande <owinocliffe10@gmail.com>
2024-03-02 11:34:41 +0300

    next

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,22 +1,45 @@
-const Hello = (props) => {  return (  
+const Hello = (props) => {  
+  console.log(props);
+  return (  
     <div>
-      <p>Hello {props.name}</p>
+      <p>Hello {props.name},your are {props.age} years old</p>
       </div>
       )}
 
+      const Footer = () => {
+        return (
+          <div>
+            greeting app created by <a href='https://github.com/mluukkai'>mluukkai</a>
+          </div>
+        )
+      }
+
 function App() {
+  const name = 'James'
+  const age = 19
   console.log('Hello from component')
   const now = new Date();
   const a = 10
   const b = 20
   console.log(now,a,b);
+// rendering objects
+const friends = [
+  { name: 'Martin', age:34},
+  { name: 'Josh', age:24},
+]
+// rendering arrays
+const numbers =[4,8]
   return (
     <>
-      <div>
+      {/* <div> */}
         <p>Hello world</p>
-        <Hello name ='cliffe' />
-        <Hello name ='ibande' />
-      </div>
+        <Hello name ={name} age={age} />
+        <Hello name ='ibande' age={26 + 10} />
+        <p>{friends[0].name} is {friends[0].age}</p>
+        <p>{friends[1].name} is {friends[1].age}</p>
+        <p>{numbers}</p>
+        <Footer />
+      {/* </div> */}
     </>
   )
 }
