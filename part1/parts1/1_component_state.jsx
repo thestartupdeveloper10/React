1527af6c6e41b51788bb2bd823535fe691452828
thestartupdeveloper10 <thestartupdeveloper0@gmail.com>
2024-04-02 18:10:41 +0300

    retest

--- part1/parts1/1_component_state.jsx
+++ part1/parts1/1_component_state.jsx
@@ -2,6 +2,7 @@ const Hello = ({ name, age }) => {
     // const name = props.name 
     // const age = props.age
     // const { name, age } = props
+    
     const bornYear = () => new Date().getFullYear() - age 
         
     console.log({ name, age });
@@ -9,12 +10,14 @@ const Hello = ({ name, age }) => {
       <div>
         <p>Hello {name},your are {age} years old</p>
         <p>You were born on {bornYear()}</p>
+       
         </div>
         )}
 
         const App = () => {
             const name = 'James'
             const age = 19
+            
             return (
               <div>
                 <Hello name ={name} age={age} />
