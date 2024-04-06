4b493dec41cc0d09354bdc546023de52bf63c4a7
thestartupdeveloper10 <thestartupdeveloper0@gmail.com>
2024-04-06 23:38:05 +0300

    new

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,30 +1,110 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import noteService from './services/notes'
+import Note from './components/Note'
+import Notification from './components/Notification'
+import Footer from './components/Footer'
 
-const Button =({handleClick,text}) =>{
-  return (
-    <button onClick={handleClick}>{text}</button>
-  )
 
-}
 
-function App() {
-  const [value, setValue] = useState(10)
 
-  // defining function outside
-  const handleClick = () => {    
-    console.log('clicked the button')  
-      setValue(0)
+const App = () => {
+  const [notes, setNotes] = useState(null)
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState(null)
+
+  useEffect(() => {
+    console.log('effect')
+    noteService
+      .getAll()
+      .then(initialNotes => {
+        console.log('promise fulfilled')
+        setNotes(initialNotes)
+      })
+
+     
+
+  }, [])
+
+  //  conditional rendering
+// do not render anything if notes is still null
+  if (!notes) { 
+    return null 
+  }
+  
+  console.log('render', notes.length, 'notes')
+
+  const addNote = (event) => {
+    event.preventDefault() 
+    console.log('button clicked', event.target)
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      // id: notes.length + 1,
+    }
+    noteService
+    .create(noteObject)
+    .then((response)=>{
+      console.log(response)
+    })
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
     }
 
+    const handleNoteChange = (event) => {    
+    console.log(event.target.value)
+    setNewNote(event.target.value)  
+    }
+    const notesToShow = showAll?notes: notes.filter(note => note.important === true)
 
-  return (
-    <>
-      <div>
-      {value}
-      {/* <button onClick={() => setValue(0)}>reset to zero</button> */}
-     <Button handleClick={handleClick} text="reset to zero" />
+
+    // toggle importance
+    const toggleImportanceOf = (id) => {   
+       console.log(`importance of ${id} needs to be toggled`) 
+      //  const url = `http://localhost:3001/notes/${id}`
+       const note = notes.find(n => n.id === id)
+       const changedNote = { ...note, important: !note.important }
+     
+       noteService
+       .update(id,changedNote)
+       .then(response => {
+         setNotes(notes.map(note => note.id !== id ? note : response.data))
+       })
+       .catch(error => {
+        console.log(error)
+        setErrorMessage(`Note '${note.content}' was already removed from server`)       
+        setTimeout(() => {setErrorMessage(null)}, 5000)
+        setNotes(notes.filter(n => n.id !== id))
+      })
+      }
+  
+  
+  
+    return (
+    <div>
+      <h1>Notes</h1>
+      < Notification message={errorMessage} />
+      <ul>
+        {/* {notes.map(note => 
+          <Note key={note.id} note={note} />
+        )} */}
+        {notesToShow.map(note =>
+        <Note 
+        key={note.id} 
+        note={note}
+        toggleImportance={() => toggleImportanceOf(note.id)}
+        />
+        )}
+      </ul>
+      <button onClick={() => setShowAll(!showAll)}>
+        show {showAll ? 'important' : 'all' }
+        </button>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange}/>
+        <button type="submit">save</button>
+        </form>
+        <Footer />
     </div>
-    </>
   )
 }
 
--- part1/src/services/notes.js
+++ part1/src/services/notes.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = '/api/notes'
+const baseUrl = 'http://localhost:3001/notes'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
